d0f33bda9fda1417dbd104d81b80fdeac19b46a0
Fyrope83 <0502741@schoolsnet.act.edu.au>
2026-07-01 21:13:00 +1000

    Finished Cleanup

--- scenes/hit_marker.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _ready() -> void:
-	pass # Replace with function body.
-
-func _process(delta: float) -> void:
-	self.modulate.a = max(0, self.modulate.a - (delta * 5)) # -5/s, clampi 0
-	if self.modulate.a == 0:
-		self.queue_free()
