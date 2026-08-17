0e9435f4249687bd0bbf3472a78bc5eb3e544eba
rhhhys <0328198@schoolsnet.act.edu.au>
2026-08-18 09:31:02 +1000

    removed redundancies and improved naming conventions in mp teams system

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -86,8 +86,8 @@ func add_player(peer_id):
 	
 	var group_counts = {get_tree().get_node_count_in_group("Team1"): "Team1", get_tree().get_node_count_in_group("Team2"): "Team2"}
 	var smallest_team = group_counts[min(get_tree().get_node_count_in_group("Team1"), get_tree().get_node_count_in_group("Team2"))]
-	apply_team(peer_id, 1 if smallest_team == "Team1" else 2) # host must skip straight to apply_team since they need to add the peer to their group so others can replicate
-	upd_teams.rpc(get_tree().get_nodes_in_group("Team1").map(func(node): return node.name), get_tree().get_nodes_in_group("Team2").map(func(node): return node.name)) # converts nodes in each team to peer ids before sending rpc
+	apply_team(peer_id, smallest_team) # host must skip straight to apply_team since they need to add the peer to their group so others can replicate
+	filter_loaded_players.rpc(get_tree().get_nodes_in_group("Team1").map(func(node): return node.name), get_tree().get_nodes_in_group("Team2").map(func(node): return node.name)) # converts nodes in each team to peer ids before sending rpc
 	
 	# gets synced via mp synchronizer node
 	$CanvasLayer/HUD/Team1.text = "Team1: " + str(get_tree().get_node_count_in_group("Team1")) + " players"
@@ -103,8 +103,8 @@ func remove_player(peer_id):
 	$CanvasLayer/HUD/Team1.text = "Team1: " + str(get_tree().get_node_count_in_group("Team1")) + " players"
 	$CanvasLayer/HUD/Team2.text = "Team2: " + str(get_tree().get_node_count_in_group("Team2")) + " players"
 
-@rpc("call_local")
-func upd_teams(team1, team2): # reload teams for everyone upon new peer connecting, ignore peers who are already loaded
+@rpc
+func filter_loaded_players(team1, team2): # reload teams for everyone upon new peer connecting, ignore peers who are already loaded
 	# filter out players who are already loaded locally (sorry for the lambda slop)
 	var unloaded_team1_players = team1.filter(func(id): return not get_tree().get_nodes_in_group("Team1").map(func(node): return node.name).has(id))
 	var unloaded_team2_players = team2.filter(func(id): return not get_tree().get_nodes_in_group("Team2").map(func(node): return node.name).has(id))
@@ -115,12 +115,12 @@ func upd_teams(team1, team2): # reload teams for everyone upon new peer connecti
 	for ids in unloaded_team2_players:
 		apply_team(ids, 2)
 
-func apply_team(plr_id, team): # keeps upd_teams relatively dry and resolves host group conflict
+func apply_team(plr_id, team): # keeps filter_loaded_players relatively dry and resolves host group conflict
 	var player_node = get_node(str(plr_id))
 	var plr_mat = player_node.get_node("MeshInstance3D").get_active_material(0).duplicate()
-	plr_mat.albedo_color = Color(1, 0, 0) if team == 1 else Color(0, 0, 1)
+	plr_mat.albedo_color = Color(1, 0, 0) if team == "Team1" else Color(0, 0, 1)
 	player_node.get_node("MeshInstance3D").set_surface_override_material(0, plr_mat)
-	player_node.add_to_group("Team%s" % team)
+	player_node.add_to_group(team)
 
 func update_health_bar(health_value):
 	health_bar.value = health_value
