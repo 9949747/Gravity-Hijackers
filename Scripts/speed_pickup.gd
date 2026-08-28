423b03a04f93adc52bf588c39798c283b6cf36f9
9949747 <9949747@schoolsnet.act.edu.au>
2026-08-28 15:29:30 +1000

    Speed Pickup group added

--- Scripts/speed_pickup.gd
+++ Scripts/speed_pickup.gd
@@ -4,7 +4,7 @@ signal speed_pickup_pickedup(int)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	add_to_group("speed_pickups")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
